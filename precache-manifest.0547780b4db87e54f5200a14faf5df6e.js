22e6ed2cde638f0b891f3d28b2fe8b01b52c825c
ChanHyLee <dannychh@khu.ac.kr>
2020-09-26 19:07:38 +0900

    Updates

---
+++ precache-manifest.0547780b4db87e54f5200a14faf5df6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d71a23cb58727ff650a02c3123e424",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4a60586e8eafa562e4f1",
+    "url": "/movie_app/static/css/main.3623ea7a.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a60586e8eafa562e4f1",
+    "url": "/movie_app/static/js/main.7d49fa2d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
